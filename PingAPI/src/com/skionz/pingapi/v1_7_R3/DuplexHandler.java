09f77a002d450630c85ff57b1409bda772055fad
Skionz <henryanderson1@hotmail.com>
2015-02-10 20:15:07 -0600

    Added support for 1.7.9

---
+++ PingAPI/src/com/skionz/pingapi/v1_7_R3/DuplexHandler.java
@@ -0,0 +1,88 @@
+package com.skionz.pingapi.v1_7_R3;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import net.minecraft.server.v1_7_R3.ChatComponentText;
+import net.minecraft.server.v1_7_R3.ChatSerializer;
+import net.minecraft.server.v1_7_R3.PacketStatusOutPong;
+import net.minecraft.server.v1_7_R3.PacketStatusOutServerInfo;
+import net.minecraft.server.v1_7_R3.ServerPing;
+import net.minecraft.server.v1_7_R3.ServerPingPlayerSample;
+import net.minecraft.server.v1_7_R3.ServerPingServerData;
+import net.minecraft.util.com.mojang.authlib.GameProfile;
+import net.minecraft.util.io.netty.channel.ChannelDuplexHandler;
+import net.minecraft.util.io.netty.channel.ChannelHandlerContext;
+import net.minecraft.util.io.netty.channel.ChannelPromise;
+
+import com.skionz.pingapi.PingAPI;
+import com.skionz.pingapi.PingEvent;
+import com.skionz.pingapi.PingListener;
+import com.skionz.pingapi.PingReply;
+import com.skionz.pingapi.reflect.ReflectUtils;
+
+public class DuplexHandler extends ChannelDuplexHandler {
+	private Field serverPingField = ReflectUtils.getFirstFieldByType(PacketStatusOutServerInfo.class, ServerPing.class);
+	private PingEvent event;
+	
+	@Override
+	public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+		if(msg instanceof PacketStatusOutServerInfo) {
+			PacketStatusOutServerInfo packet = (PacketStatusOutServerInfo) msg;
+			PingReply reply = this.constructReply(packet, ctx);
+			PingEvent event = new PingEvent(reply);
+			for(PingListener listener : PingAPI.getListeners()) {
+				listener.onPing(event);
+			}
+			this.event = event;
+			if(!event.isCancelled()) {
+				super.write(ctx, this.constructorPacket(reply), promise);
+			}
+			return;
+		}
+		if(msg instanceof PacketStatusOutPong) {
+			if(this.event != null && this.event.isPongCancelled()) {
+				return;
+			}
+		}
+		super.write(ctx, msg, promise);
+	}
+
+	private PingReply constructReply(PacketStatusOutServerInfo packet, ChannelHandlerContext ctx) {
+		try {
+			ServerPing ping = (ServerPing) serverPingField.get(packet);
+			String motd = ChatSerializer.a(ping.a());
+			int max = ping.b().a();
+			int online = ping.b().b();
+			int protocolVersion = ping.c().b();
+			String protocolName = ping.c().a();
+			GameProfile[] profiles = ping.b().c();
+			List<String> list = new ArrayList<String>();
+			for(int i = 0; i < profiles.length; i++) {
+				list.add(profiles[i].getName());
+			}
+			PingReply reply = new PingReply(ctx, motd, online, max, protocolVersion, protocolName, list);
+			return reply;
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	private PacketStatusOutServerInfo constructorPacket(PingReply reply) {
+		GameProfile[] sample = new GameProfile[reply.getPlayerSample().size()];
+		List<String> list = reply.getPlayerSample();
+		for(int i = 0; i < list.size(); i++) {
+			sample[i] = new GameProfile(UUID.randomUUID(), list.get(i));
+		}
+		ServerPingPlayerSample playerSample = new ServerPingPlayerSample(reply.getMaxPlayers(), reply.getOnlinePlayers());
+        playerSample.a(sample);
+        ServerPing ping = new ServerPing();
+        ping.setMOTD(new ChatComponentText(reply.getMOTD()));
+        ping.setPlayerSample(playerSample);
+        ping.setServerInfo(new ServerPingServerData(reply.getProtocolName(), reply.getProtocolVersion()));
+        return new PacketStatusOutServerInfo(ping);
+	}
+}
